fea11e68d93125448fed6ea855044f32fee196e7
Addie Okoro <adinduokoro95@gmail.com>
2023-10-14 02:32:05 -0400

    Welcome
    Still working on the welcome section.

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="section">
+      <div className="welcome">
+        <div className="welcome__left">
+          <div className="welcome__leftTop">
+
+          </div>
+          <div className="welcome__leftBottom">
+            
+          </div>
+        </div>
+        <div className="welcome__right">
+          <div className="welcome__info">
+            <h1 className="welcome__title">WELCOME</h1>
+            <p className="welcome__text">
+              Discover London's Bright Beginnings, your trusted childcare choice
+              in Baltimore, Maryland. We're dedicated to providing personalized
+              educational experiences for children and families, fostering a
+              lifelong love for learning. With experienced teachers and proven
+              strategies, our specialized programs cater to ages 6 weeks to 12
+              years. Join us for a journey where your child's growth is our top
+              priority.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Hero from '../components/hero/Hero'
-import "../styles/home.css"
+import React from "react";
+import Hero from "../components/hero/Hero";
+import "../styles/home.css";
+import Welcome from "../components/welcome/Welcome";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      {/* Welcome */}
+      <Welcome />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
