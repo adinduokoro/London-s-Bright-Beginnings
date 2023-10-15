00794aadd52672b3a5413177129b785d3da0e1d6
Addie Okoro <adinduokoro95@gmail.com>
2023-10-15 12:34:23 -0400

    Program
    Program section is complete. Need to add the links and itll be ready to go.

--- src/components/programs/Programs.jsx
+++ src/components/programs/Programs.jsx
@@ -7,12 +7,16 @@ const Programs = () => {
       {programs.map((program, index) => {
         return (
           <div className="programs__cardContainer" key={index}>
-            <div className="programs__cardInfo">
-              <span>Programs for</span>
-              <h1 style={{color: program.color}}>{program.title}</h1>
-              <div className="cardLine"></div>
-              <p>{program.paragraph}</p>
-              <a href={program.path}>READ MORE</a>
+            <div className="programs__card">
+              <div className="programs__cardHeader">
+                <span>Programs for</span>
+                <h1 style={{ color: program.color }}>{program.title}</h1>
+                <div className="cardLine"></div>
+              </div>
+              <div className="programs__cardInfo">
+                <p>{program.paragraph}</p>
+                <a href={program.path} style={{ color: program.color }}>READ MORE</a>
+              </div>
             </div>
           </div>
         );
