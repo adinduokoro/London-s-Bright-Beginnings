be0ce24b77f9f9e39b5db9e3a264170f330b0534
Addie Okoro <adinduokoro95@gmail.com>
2023-10-15 10:48:09 -0400

    Programs
    Created the data for the programs.

--- src/components/programs/Programs.jsx
+++ src/components/programs/Programs.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import { programs } from "./data";
 
 const Programs = () => {
   return (
-    <div className="section">
-      <div className="programs__cardContainer">
-        <div className="programs__cardInfo">
-          <span>Programs for</span>
-          <h1>INFANTS</h1>
-          <div className="cardLine"></div>
-          <p>
-            We offer a secure and nurturing space for your precious little one
-            to bloom with care.
-          </p>
-          <a href="/">READ MORE</a>
-        </div>
-      </div>
+    <div className="program section">
+      {programs.map((program, index) => {
+        return (
+          <div className="programs__cardContainer" key={index}>
+            <div className="programs__cardInfo">
+              <span>Programs for</span>
+              <h1 style={{color: program.color}}>{program.title}</h1>
+              <div className="cardLine"></div>
+              <p>{program.paragraph}</p>
+              <a href={program.path}>READ MORE</a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/components/programs/data.js
@@ -0,0 +1,26 @@
+export const programs = [
+  {
+    title: "INFANTS",
+    color: "var(--primaryColorBlue)",
+    paragraph: "We offer a secure and nurturing space for your precious little one to bloom with care.",
+    path: "/",
+  },
+  {
+    title: "TODDLER",
+    color: "var(--primaryColorPink)",
+    paragraph: "Exploration of play, imaginative arts, language growth, and sensory development.",
+    path: "/",
+  },
+  {
+    title: "PRESCHOOL",
+    color: "var(--primaryColorYellow)",
+    paragraph: "Nurturing vital critical thinking and problem-solving skills for academic success.",
+    path: "/",
+  },
+  {
+    title: "SCHOOL-AGE",
+    color: "var(--primaryColorGreen)",
+    paragraph: "A secure setting for extra study and homework time, available before and after school hours.",
+    path: "/",
+  },
+]
