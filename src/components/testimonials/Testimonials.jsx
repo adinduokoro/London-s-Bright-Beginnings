3e075b59b017e0184187ba5a98de35404897d9db
Addie Okoro <adinduokoro95@gmail.com>
2023-10-22 12:05:29 -0400

    Testimonial
    Testimonials section is almost done. Just need to work on the card animation.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./testimonials.css";
 import FamilyPhoto from "../../assets/family-photo.png";
 import GraduationCap from "../../assets/graduation-cap.png";
+import Chalk from "../../assets/chalk.png";
 
 const Testimonials = () => {
   return (
@@ -10,10 +11,16 @@ const Testimonials = () => {
         <img className="testimonials__family" src={FamilyPhoto} alt="" />
         <img className="testimonials__cap" src={GraduationCap} alt="" />
         <div className="testimonials__cards">
-          <div className="testimonials__card">
+          <div className="testimonials__cardsh3">
+            <img src={Chalk} alt="" />
             <h3>parent testimonials</h3>
-            <p>"Highly recommended! Wonderful teachers and experienced staff make this preschool one of the best around."</p>
-            <h6>- Jane D.</h6>
+          </div>
+          <div className="testimonials__cardInfo">
+            <p>
+              "Highly recommended! Wonderful teachers and experienced staff make
+              this preschool one of the best around."
+            </p>
+            <h6>-Jane D.</h6>
           </div>
         </div>
       </div>
