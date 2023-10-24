d3659dd1600176b58a7919de5f6644b2c5f34c54
Addie Okoro <adinduokoro95@gmail.com>
2023-10-24 13:04:37 -0400

    Working on footer section.

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Location = () => {
-  return (
-    <div className='section__full'>
-      Location
-    </div>
-  )
-}
+  const iframeStyle = {
+    width: "100%",
+    height: "100%",
+    border: "0",
+    overflow: "hidden",
+  };
 
-export default Location
+  return (
+    <div className="section__full">
+      <>
+        <div style={{ width: "100%" }}>
+          <iframe
+            style={iframeStyle}
+            title="Google Map"
+            src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=Baltimore,%20Maryland+(London's%20Bright%20Beginnings)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.maps.ie/population/">Find Population on Map</a>
+          </iframe>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default Location;
