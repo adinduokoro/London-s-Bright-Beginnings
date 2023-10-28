0db76b3b6fccf52672258dbbd22fc9915df9986c
Addie Okoro <adinduokoro95@gmail.com>
2023-10-28 13:34:26 -0400

    Testimonial section is done.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ import familyPhoto from "../../assets/family-photo.png";
 import graduationCap from "../../assets/graduation-cap.png";
 import chalk from "../../assets/chalk.png";
 import { testimonials } from "./data";
-import chalkboard from "../../assets/Chalkboard.jpg"
+import chalkboard from "../../assets/Chalkboard.jpg";
 
 const Testimonials = () => {
   const [current, setCurrent] = useState(0);
@@ -18,39 +18,41 @@ const Testimonials = () => {
   }, [current]);
 
   return (
-    <div className="section__full" style={{backgroundImage : `url(${chalkboard})`}}>
+    <div
+      className="section__full"
+      style={{ backgroundImage: `url(${chalkboard})` }}
+    >
       <div className="section">
         <div className="testimonials">
-        <img className="testimonials__family" src={familyPhoto} alt="" />
-        <img className="testimonials__cap" src={graduationCap} alt="" />
-        <div className="testimonials__cards">
-          <div className="testimonials__cardsh3">
-            <img src={chalk} alt="" />
-            <h3>parent testimonials</h3>
+          <img className="testimonials__family" src={familyPhoto} alt="" />
+          <img className="testimonials__cap" src={graduationCap} alt="" />
+          <div className="testimonials__cards">
+            <div className="testimonials__cardsh3">
+              <img src={chalk} alt="" />
+              <h3>parent testimonials</h3>
+            </div>
+            <div className="testimonials__slider">
+              {testimonials.map((review, index) => {
+                return (
+                  <div
+                    className={
+                      index === current
+                        ? "testimonials__cardInfo active"
+                        : "testimonials__cardInfo"
+                    }
+                    key={index}
+                  >
+                    {index === current && (
+                      <>
+                        <p>{review.response}</p>
+                        <h6>-{review.customer}</h6>
+                      </>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="testimonials__slider">
-            {testimonials.map((review, index) => {
-              return (
-                <div
-                  className={
-                    index === current
-                      ? "testimonials__cardInfo active"
-                      : "testimonials__cardInfo"
-                  }
-                  key={index}
-                >
-                  {index === current && (
-                    <>
-                      <p>{review.response}</p>
-                      <h6>-{review.customer}</h6>
-                    </>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
         </div>
       </div>
     </div>
