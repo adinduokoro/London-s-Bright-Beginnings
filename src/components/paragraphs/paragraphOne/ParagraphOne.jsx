81407ea0df29d7f82c08fd74ee21629ddb124530
Addie Okoro <adinduokoro95@gmail.com>
2023-11-14 07:11:30 -0500

    Paragraph1
    Finished the layout for paragraph1. Need to work on 2 and 3.

--- src/components/paragraphs/paragraphOne/ParagraphOne.jsx
+++ src/components/paragraphs/paragraphOne/ParagraphOne.jsx
@@ -2,34 +2,20 @@ import React from "react";
 import "./paragraphOne.css";
 import RightArrow from "../../../assets/RightArrow.svg";
 
-const ParagraphOne = ({title, body}) => {
+const ParagraphOne = ({ title, body, bulletPoints }) => {
   return (
     <div className="paragraphOne">
       <h3 className="title-text">{title}</h3>
-      <p className="body-text">
-        {body}
-      </p>
+      <p className="body-text">{body}</p>
       <ul>
-        <li>
-          <img src={RightArrow} alt="" />
-          Lorem ipsum dolor sit amet consectetur. Et nunc elit eu a sed congue
-          sem fermentum.
-        </li>
-        <li>
-          <img src={RightArrow} alt="" />
-          Lorem ipsum dolor sit amet consectetur. Nascetur viverra non vulputate
-          dui ac pellentesque.
-        </li>
-        <li>
-          <img src={RightArrow} alt="" />
-          Lorem ipsum dolor sit amet consectetur. Turpis nec tortor neque metus
-          lectus dictumst nunc.
-        </li>
-        <li>
-          <img src={RightArrow} alt="" />
-          Lorem ipsum dolor sit amet consectetur. Turpis nec tortor neque metus
-          lectus dictumst nunc.
-        </li>
+        {bulletPoints.map((bullets, index) => {
+          return (
+            <li key={index}>
+              <img src={RightArrow} alt="" />
+              {bullets}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,8 @@ import ParagraphTwo from "../components/paragraphs/paragraphTwo/ParagraphTwo";
 import ParagraphImg from "../components/paragraphs/paragraphImg/ParagraphImg";
 import ParagraphThree from "../components/paragraphs/paragraphThree/ParagraphThree";
 import Header from "../components/paragraphs/header/Header";
-import aboutUsImg from '../assets/AboutUs.jpg'
-import directorImg from '../assets/Person.jpg'
+import aboutUsImg from "../assets/AboutUs.jpg";
+import directorImg from "../assets/Person.jpg";
 
 const About = () => {
   return (
@@ -17,12 +17,23 @@ const About = () => {
         <div className="about__container" style={{ margin: "0 1.5rem" }}>
           <Header title={"About Us"} />
           <div className="about__content section">
-            <ParagraphImg img={aboutUsImg}/>
-            <ParagraphOne title={"Our Vision"} body={"The Vision of London’s Bright Beginnings is to be a place where we:"} bullet-points={""}/>
+            <ParagraphImg img={aboutUsImg} />
+            <ParagraphOne
+              title={"Our Vision"}
+              body={
+                "The Vision of London’s Bright Beginnings is to be a place where we:"
+              }
+              bulletPoints={[
+                "Lorem ipsum dolor sit aet consectetur. Et nunc elit eu a sed congue sem fermentum.",
+                "Lorem ipsum dolor sit amet consectetur. Nascetur viverra non vulputate dui ac pellentesque.",
+                "Lorem ipsum dolor sit amet consectetur. Turpis nec tortor neque metus lectus dictumst nunc.",
+                "Lorem ipsum dolor sit amet consectetur. Turpis nec tortor neque metus lectus dictumst nunc.",
+              ]}
+            />
             <ParagraphTwo />
           </div>
           <div className="our__director section">
-            <ParagraphImg img={directorImg}/>
+            <ParagraphImg img={directorImg} />
             <ParagraphThree />
           </div>
         </div>
