261486ae43f8bdb6e044b99da427952074e533de
Addie Okoro <adinduokoro95@gmail.com>
2023-11-27 12:39:26 -0500

    Update from break

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Banner from '../components/banner/Banner'
 import contactBanner from "../assets/ContactBanner.png"
+import Header from '../components/paragraphs/header/Header'
 
 const Contact = () => {
   return (
     <div className='contact'>
       <Banner pageTitle={"Contact Us"} bannerImg={contactBanner} />
+      <div className="content" style={{ marginBottom: "3.12rem" }}>
+        <div className="contact__container" style={{ margin: "0 1.5rem" }}>
+          <Header title={"Contact"}/>
+          <div className="contact__content section">
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Curriculum.jsx
+++ src/pages/Curriculum.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Banner from '../components/banner/Banner'
 import curriculumBanner from "../assets/CurriculumBanner.png"
+import Header from '../components/paragraphs/header/Header'
+import ParagraphTwo from '../components/paragraphs/paragraphTwo/ParagraphTwo'
 
 const Curriculum = () => {
   return (
     <div className='curriculum'>
       <Banner pageTitle={"Curriculum"} bannerImg={curriculumBanner} />
+      <div className="content" style={{ marginBottom: "3.12rem" }}>
+        <div className="curriculum__container" style={{ margin: "0 1.5rem" }}>
+          <Header title={"Curriculum"}/>
+          <div className="curriculum__content section">
+            <ParagraphTwo 
+            title={"Our Curriculum Focus"}
+            body={
+              "Lorem ipsum dolor sit amet consectetur. Et mollis quis pretium at ultrices est quis id. Habitasse tristique ut erat habitasse in sit. Sit suspendisse ullamcorper pellentesque at vulputate sit velit. Feugiat tellus quam tortor viverra tellus dignissim arcu auctor eget. Consectetur id turpis eget porta ibendum posuere leo sed. Leo amet dolor metus tristique elementum in. Ultricies ut scelerisque in in dictumst id egestas. Orci sed dolor duis vulputate in enim malesuada mollis quisque sollicitudin. Leo amet dolor metus tristique elementum in. Ultricies ut scelerisque in in dictumst id egestas. Orci sed dolor duis vulputate in enim malesuada mollis quisque sollicitudin Leo amet dolor metus tristique elementum in. Ultricies ut scelerisque in in dictumst id egestas. Orci sed dolor duis vulputate in enim malesuada mollis quisque sollicitudin.."
+            }
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
