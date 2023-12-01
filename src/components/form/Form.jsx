911253498e108334e13b934762484ed7a99d4a45
Addie Okoro <adinduokoro95@gmail.com>
2023-12-01 11:41:47 -0500

    Update

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,11 +4,59 @@ import "./form.css";
 const Form = () => {
   return (
     <div className="section">
-      <form>
-        <div className="form">
-          <h2>Inquiries/Request a Tour</h2>
+      <div className="form">
+        <div className="form__content">
+          <h2 className="head-text">Inquiries</h2>
+          <form className="body-text">
+            <label>
+              Parent's Name
+              <input type="text" name="pName" />
+            </label>
+            <label>
+              Parent's Phone
+              <input type="number" name="Phone" />
+            </label>
+            <label>
+              Parent's Email
+              <input type="email" name="Email" />
+            </label>
+            <label>
+              Child's Name
+              <input type="text" name="cName" />
+            </label>
+            <label>
+              Child's Birthdate
+              <input type="date" name="birthdate" />
+            </label>
+            <label>
+              Interested In
+              <select>
+                <option value="" disabled selected hidden>
+                  {" "}
+                  Please Choose...
+                </option>
+                <option value="Ford">Ford</option>
+                <option value="Volvo">Volvo</option>
+                <option value="Fiat">Fiat</option>
+              </select>
+            </label>
+            <label>
+              Anticipated Start Date
+              <input type="text" name="startDate" />
+            </label>
+            <label>
+              Comments
+              <textarea
+                placeholder="Say something"
+                name="comment"
+                rows={4}
+                cols={40}
+              />
+            </label>
+            <input type="submit" />
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
