926ba3f7e7f5f55d1077d40d7c7934d02f30b22d
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-12-15 00:21:15 -0500

    Update Form.jsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,29 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./form.css";
 import emailjs from "@emailjs/browser";
 
 const Form = () => {
   const form = useRef();
+  const [pName, setPName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [cName, setCName] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [interest, setInterest] = useState("");
+
+  const [startDate, setStartDate] = useState("");
+  const [message, setMessage] = useState("");
+
+  const initialState = () => {
+    setPName("");
+    setPhone("");
+    setEmail("");
+    setCName("");
+    setBirthDate("");
+    setInterest("");
+    setStartDate("");
+    setMessage("");
+  };
 
   async function sendEmail(e) {
     e.preventDefault();
@@ -15,11 +35,13 @@ const Form = () => {
         form.current,
         process.env.REACT_APP_PUBLIC_KEY
       );
-      console.log(result);
+      initialState();
+      console.log(form.current);
     } catch (error) {
+      initialState();
       console.log("not working");
     }
-  };
+  }
 
   return (
     <div className="section">
@@ -30,37 +52,83 @@ const Form = () => {
             <>
               <label>
                 Parent's Name
-                <input type="text" name="pName" />
+                <input
+                  type="text"
+                  name="pName"
+                  maxLength={21}
+                  required
+                  value={pName}
+                  onChange={(e) => setPName(e.target.value)}
+                />
               </label>
               <label>
                 Parent's Phone
-                <input type="number" name="phone" />
+                <input
+                  type="number"
+                  name="phone"
+                  maxLength={21}
+                  required
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
               </label>
               <label>
                 Parent's Email
-                <input type="email" name="email" />
+                <input
+                  type="email"
+                  name="email"
+                  maxLength={21}
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </label>
 
               <label>
                 Child's Name
-                <input type="text" name="cName" />
+                <input
+                  type="text"
+                  name="cName"
+                  maxLength={21}
+                  value={cName}
+                  onChange={(e) => setCName(e.target.value)}
+                />
               </label>
               <label>
                 Child's Birthdate
-                <input type="date" name="birthdate" />
+                <input
+                  type="date"
+                  name="birthDate"
+                  required
+                  value={birthDate}
+                  onChange={(e) => setBirthDate(e.target.value)}
+                />
               </label>
               <label>
                 Interested In
-                <select name="interest" defaultValue="">
+                <select
+                  name="interest"
+                  value={interest}
+                  required
+                  onChange={(e) => setInterest(e.target.value)}
+                >
                   <option disabled value=""></option>
-                  <option value="Option 1">Option 1</option>
-                  <option value="Option 2">Option 2</option>
-                  <option value="Option 3">Option 3</option>
+                  <option value="Option 1">Infant</option>
+                  <option value="Option 2">Toddler</option>
+                  <option value="Option 3">Preschool</option>
+                  <option value="Option 3">School Age</option>
                 </select>
               </label>
               <label>
                 Anticipated Start Date
-                <input type="text" name="startDate" />
+                <input
+                  type="text"
+                  name="startDate"
+                  maxLength={21}
+                  required
+                  value={startDate}
+                  onChange={(e) => setStartDate(e.target.value)}
+                />
               </label>
               <label>
                 Comments
@@ -69,6 +137,10 @@ const Form = () => {
                   name="message"
                   rows={8}
                   cols={40}
+                  maxLength={200}
+                  required
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 />
               </label>
             </>
